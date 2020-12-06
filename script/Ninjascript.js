5ea6ca33fb611adf58f0aa085a7f0ad2d8f6ebe1
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2020-12-05 19:05:54 -0500

    Create Ninjascript.js

---
+++ script/Ninjascript.js
@@ -0,0 +1,12 @@
+var pokemon = [{"name":"Squirtle","type":"water"},{"name": "Magicarp", "type":"water"},{"name":"Blastoise","type":"water"}];
+var div = document.getElementById('first');
+div.addEventListener('click', function(){
+  
+
+alert(for (i = 0; i < pokemon.length; i++) if (pokemon[i].type == "electric"){ {
+pokemon[i].name + ' ' + pokemon[i].type
+}
+Else {alert("you do not have any electric type pokemon"))}
+})
+
+});
