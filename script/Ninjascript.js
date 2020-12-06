c311577a7de78f033e67b63402e3c033790b8ca3
Aaron Lilly <aaronlilly84@gmail.com>
2020-12-05 21:58:40 -0500

    while insted

--- script/Ninjascript.js
+++ script/Ninjascript.js
@@ -1,12 +1,44 @@
+// var pokemon = [{"name":"Squirtle","type":"water"},{"name": "Magicarp", "type":"water"},{"name":"Blastoise","type":"water"}];
+// var div = document.getElementById('first');
+
+
+// div.addEventListener('click', function(){
+// 	 // for(i= 0; i < pokemon.length; i++){
+// 		// 			 	if (pokemon[i].type == "electric"){
+// 		// 			alert(pokemon[i].name + ' ' + pokemon[i].type)
+// 		// 		}
+// 		// 		else {	i=pokemon.length+1;
+// 		// 			alert("you don't have any electric type pokemon")}
+// 		// 			}
+
+
+// 		var i = 0;
+// 			var text = "";
+// 			while (pokemon[i]) {
+//  			 text += pokemon[i].name + "\n";
+//   				i++;}
+//   				alert(text);
+				
+// 			});
+
+
+
+
+
+
 var pokemon = [{"name":"Squirtle","type":"water"},{"name": "Magicarp", "type":"water"},{"name":"Blastoise","type":"water"}];
 var div = document.getElementById('first');
 div.addEventListener('click', function(){
-  
 
-alert(for (i = 0; i < pokemon.length; i++) if (pokemon[i].type == "electric"){ {
-pokemon[i].name + ' ' + pokemon[i].type
-}
-Else {alert("you do not have any electric type pokemon"))}
-})
-
-});
+	var i=0;
+	var text ="";
+	if (pokemon[i].type == "electric"){
+	 while (pokemon[i]) {
+					 	
+					 		text += pokemon[i].name + "\n";
+  				i++;} 
+  				alert(text);
+				}
+				else { alert("you dont have any electric pokemon")}
+				
+				});
